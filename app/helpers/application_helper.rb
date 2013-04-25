81137a54b55aeb6ba700b915c807947428c16bc2
Michael Keenan <keenanm@ecs.csus.edu>
2013-04-24 18:22:05 -0700

    Deduplicate code in layouts.
    Added ApplicationHelper#render_parent_layout method. Used by layouts to render themselves within the
    yield block of the parent layout (defaults to 'application' layout)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+  # Provide a mechanism to render layout within a parent layout
+  def render_parent_layout(layout = 'application')
+    @view_flow.set(:layout,output_buffer)
+    self.output_buffer = render(:file => "layouts/#{layout}")
+  end
 end
